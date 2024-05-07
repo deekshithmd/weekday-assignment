a5febd1ebedcab6e47e7ba4e4b7c22b0c3f7f098
deekshithmd <deekshithmogra@gmail.com>
2024-05-07 23:18:08 +0530

    integrated api

--- src/App.js
+++ src/App.js
@@ -1,9 +1,51 @@
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+
+  const [jobs, setJobs] = useState([])
+
+  const myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
+
+  const body = JSON.stringify({
+    "limit": 10,
+    "offset": 0
+  });
+
+  const requestOptions = {
+    method: "POST",
+    headers: myHeaders,
+    body
+  };
+
+  useEffect(() => {
+    fetch("https://api.weekday.technology/adhoc/getSampleJdJSON", requestOptions)
+      .then((response) => response.text())
+      .then((result) => {
+        setJobs(JSON.parse(result)?.jdList)
+      })
+      .catch((error) => console.error(error));
+  }, [])
+
   return (
     <div className="App">
-      Weekday
+      {
+        jobs.map(job =>
+          <div key={job?.jdUid}>
+
+            <div>
+              <img src={job?.logoUrl} alt={job?.companyName} />
+              <span>{job?.companyName}</span>
+            </div>
+            <span>{job?.jobRole}</span>
+            <div>
+              <p>{job?.jobDetailsFromCompany}</p>
+            </div>
+            <a href={job?.jdLink} rel='noreferrer' target='_blank'>Apply</a>
+          </div>
+        )
+      }
     </div>
   );
 }
