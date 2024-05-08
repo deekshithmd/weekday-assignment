4054e992dac50309ebe99b7f521dd3818d2532da
deekshithmd <deekshithmogra@gmail.com>
2024-05-08 18:53:41 +0530

    added infinite scroll

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { updateJobs } from './features/jobs/jobSlice';
 import './App.css';
 
 function App() {
   const { jobsList } = useSelector((state) => state.jobs);
+  const [pageLimit, setPageLimit] = useState(10);
+  const [showMoreIds, setShowMoreIds] = useState([]);
+  const endRef = useRef(null)
   const dispatch = useDispatch();
 
   const myHeaders = new Headers();
   myHeaders.append("Content-Type", "application/json");
 
   const body = JSON.stringify({
-    "limit": 10,
+    "limit": pageLimit,
     "offset": 0
   });
 
@@ -28,26 +31,46 @@ function App() {
         dispatch(updateJobs(JSON.parse(result)?.jdList))
       })
       .catch((error) => console.error(error));
-  }, [])
+  }, [pageLimit])
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      entries => {
+        if (entries[0]?.isIntersecting) {
+          setPageLimit(pageLimit => pageLimit + 10);
+        }
+      })
+    if (endRef?.current) {
+      observer.observe(endRef.current);
+    }
+
+    return () => {
+      if (endRef?.current) {
+        observer.unobserve(endRef.current);
+      }
+    }
+  }, [endRef])
 
   return (
-    <div className="App">
+    <div className="jobs-container">
       {
         jobsList.map(job =>
-          <div key={job?.jdUid}>
+          <div key={job?.jdUid} className='job-card'>
 
             <div>
-              <img src={job?.logoUrl} alt={job?.companyName} />
+              <img src={job?.logoUrl} alt={job?.companyName} className='company-logo' />
               <span>{job?.companyName}</span>
             </div>
             <span>{job?.jobRole}</span>
             <div>
-              <p>{job?.jobDetailsFromCompany}</p>
+              <p>{showMoreIds?.includes(job?.jdUid) ? job?.jobDetailsFromCompany : job?.jobDetailsFromCompany?.slice(0, 300)}</p>
+              <p onClick={() => setShowMoreIds(showMoreIds?.includes(job?.jdUid) ? [...showMoreIds?.filter(id => id !== job?.jdUid)] : [...showMoreIds, job?.jdUid])}>{showMoreIds?.includes(job?.jdUid) ? 'Show Less' : 'Show More'}</p>
             </div>
             <a href={job?.jdLink} rel='noreferrer' target='_blank'>Apply</a>
           </div>
         )
       }
+      <div ref={endRef} />
     </div>
   );
 }
